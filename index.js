f60694a5624ce8e122b02563e09be7099f43a5ca
Tyler Corbett <tyler.corbett@fast.co>
2022-04-05 12:14:22 -0500

    swapping works

--- index.js
+++ index.js
@@ -7,46 +7,52 @@ const contactInfo = [
   {
     name: "Rich",
     email: "rich@tripod.com",
-    phone: "323-555-1234",
+    phone: "323-333-1234",
   },
   {
     name: "Scott",
     email: "scott@mailinator.com",
-    phone: "323-555-1234",
+    phone: "323-444-1234",
   },
   {
     name: "Danny",
     email: "danny@hotmail.com",
-    phone: "323-555-1234",
+    phone: "323-111-1234",
   },
   {
     name: "Taka",
     email: "taka@myspace.com",
-    phone: "323-555-1234",
+    phone: "432-999-1234",
   },
   {
     name: "Tim",
     email: "tim@netscape.com",
-    phone: "323-555-1234",
+    phone: "323-331-1234",
   },
   {
     name: "Patrick",
     email: "patrick@live.com",
-    phone: "323-555-1234",
+    phone: "323-931-1234",
   },
   {
     name: "Jacques",
     email: "jacques@aol.com",
-    phone: "323-555-1234",
+    phone: "123-000-1234",
   },
-]
+];
 
 const handleSelectChange = () => {
-  const selectElementValue = document.getElementById("contact-select").value;
+  const desiredContactInfoValue =
+    document.getElementById("contact-select").value;
+  const oppositeContactInfoValue =
+    desiredContactInfoValue === "phone" ? "email" : "phone";
   const contactInfoElements = document.querySelectorAll(".contact-info");
-  console.log("contactInfoElements", contactInfoElements);
 
   contactInfoElements.forEach((element) => {
-    console.log(element.innerText, "inner text");
+    const individualContactData = contactInfo.find((contact) => {
+      console.log("contact", contact[oppositeContactInfoValue]);
+      return contact[oppositeContactInfoValue] === element.innerText;
+    });
+    element.innerText = individualContactData[desiredContactInfoValue];
   });
 };
